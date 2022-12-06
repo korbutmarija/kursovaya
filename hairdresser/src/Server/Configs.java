e449fe9c6d76f3e6a35b32c3f1f5c7f56a5637f7
Pocke <korbutmari47@gmail.com>
2022-12-06 17:03:19 +0300

    Create Configs.java

---
+++ hairdresser/src/Server/Configs.java
@@ -0,0 +1,10 @@
+package Server;
+
+public class Configs
+{
+    protected String dbHost = "127.0.0.1";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "12345";
+    protected String dbName = "hairdresser";
+}
